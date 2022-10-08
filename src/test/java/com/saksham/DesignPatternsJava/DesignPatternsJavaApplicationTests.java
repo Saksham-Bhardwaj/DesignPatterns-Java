bfd6512f7cc7fee108be5f613fe7ee90d3a5ba46
saksham <sakshambhardwaj002@gmail.com>
2022-10-09 01:13:21 +0530

    setting up base project for design patterns

---
+++ src/test/java/com/saksham/DesignPatternsJava/DesignPatternsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saksham.DesignPatternsJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
